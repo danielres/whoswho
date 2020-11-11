646d2443776bb6bb58a4ce5a9050894a1c95c729
Daniel Reszka <dannn.r@gmail.com>
2020-11-12 00:46:53 +0100

    api: /users/create: use real posted values

--- pages/api/users/create.ts
+++ pages/api/users/create.ts
@@ -1,13 +1,11 @@
 import { User } from "src/api/graphql/queries";
 
 export default async (req, res) => {
-  const rand = Math.floor(Math.random() * 1000);
-  const tempData = { name: `Rand-${rand}`, email: `rand${rand}@example.com` };
-
   try {
-    const result = await User.create(tempData);
+    const result = await User.create(JSON.parse(req.body));
     res.json(result);
   } catch (error) {
-    console.error(error);
+    console.error({ error: error.message });
+    res.json({ error: error.message });
   }
 };
