5a4ffc4ec7121163589a98455348e91eb7d1d9de
Daniel Reszka <dannn.r@gmail.com>
2020-11-12 00:45:36 +0100

    ui: queries: User: add query create

--- src/ui/queries/User.js
+++ src/ui/queries/User.js
@@ -2,3 +2,9 @@ import * as config from "../config";
 
 export const list = async () =>
   fetch(`${config.api.url}/users`).then((res) => res.json());
+
+export const create = async (values) =>
+  fetch(`${config.api.url}/users/create`, {
+    method: "POST",
+    body: JSON.stringify(values),
+  }).then((res) => res.json());
