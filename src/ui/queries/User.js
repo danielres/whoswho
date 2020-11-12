ed77d2c4c89fd5b7f577c5f2d87a4aab76c39338
Daniel Reszka <dannn.r@gmail.com>
2020-11-12 03:03:27 +0100

    ui/queries/User: create: add error handling

--- src/ui/queries/User.js
+++ src/ui/queries/User.js
@@ -7,4 +7,14 @@ export const create = async (values) =>
   fetch(`${config.api.url}/users/create`, {
     method: "POST",
     body: JSON.stringify(values),
-  }).then((res) => res.json());
+  })
+    .then((res) => {
+      if (res.ok) return res;
+      switch (res.status) {
+        case 409:
+          throw new Error("User already exists");
+        default:
+          throw new Error(`Unknown error. Code: ${res.status}`);
+      }
+    })
+    .then((res) => res.json());
