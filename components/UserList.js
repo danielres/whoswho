2279915618c48cfd6abf48556eb25f8696ff0b80
Daniel Reszka <dannn.r@gmail.com>
2020-11-12 01:23:52 +0100

    UserList: add styling

--- components/UserList.js
+++ components/UserList.js
@@ -1,3 +1,4 @@
+import formatDistance from "date-fns/formatDistance";
 import { useEffect, useState } from "react";
 import { User } from "src/ui/queries";
 
@@ -11,10 +12,31 @@ export const UserList = () => {
   return (
     <>
       {users && (
-        <ul>
+        <ul class="list-none p-0 m-auto text-gray-900">
           {users.map((user) => (
-            <li key={user.email}>
-              {user.name} {user.email}
+            <li class="inline-block flex justify-between items-center pb-4">
+              <div class="flex items-start w-2/5">
+                <div class="w-10 h-10 rounded mr-3">
+                  <div class="rounded-full h-10 w-10 bg-gray-300 m-auto"></div>
+                </div>
+
+                <div class="flex-1 overflow-hidden">
+                  <div>
+                    <span class="font-bold">{user.name}</span>
+                  </div>
+                  <p class="text-black leading-normal text-sm text-gray-500">
+                    {formatDistance(new Date(user.createdAt), new Date(), {
+                      addSuffix: true,
+                    })}
+                  </p>
+                </div>
+              </div>
+
+              <p class="w-2/5">{user.email}</p>
+
+              <label for="status" class="font-bold w-1/5 text-right">
+                Active
+              </label>
             </li>
           ))}
         </ul>
