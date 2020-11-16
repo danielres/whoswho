06c0f222fb29addec97f4e6baa36b621f5be70e7
Daniel Reszka <dannn.r@gmail.com>
2020-11-16 18:42:51 +0100

    config: fix missing entries for uploadSchema

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,8 +1,15 @@
+import path from "path";
 import { env } from "process";
 
 export const fauna = {
   graphql: {
     endpoint: env.FAUNA_GRAPHQL_ENDPOINT,
+    import: {
+      endpoint: env.FAUNA_GRAPHQL_IMPORT_ENDPOINT,
+    },
+    schema: {
+      path: path.join(process.cwd(), "src", "api", "schema.graphql"),
+    },
   },
   keys: {
     admin: env.FAUNA_KEYS_ADMIN,
