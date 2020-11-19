01b924673365adb900107c0d1eebd0ca67d0f58d
Daniel Reszka <dannn.r@gmail.com>
2020-11-19 09:28:34 +0100

    env: use js format

---
+++ .env.example.js
@@ -0,0 +1,6 @@
+module.exports = {
+  FAUNA_GRAPHQL_ENDPOINT: "https://graphql.fauna.com/graphql",
+  FAUNA_GRAPHQL_IMPORT_ENDPOINT: `https://graphql.fauna.com/import`,
+  FAUNA_KEYS_SERVER: "changeme",
+  FAUNA_KEYS_ADMIN: "changeme",
+};
