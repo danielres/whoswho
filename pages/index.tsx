c6b90ecad7c5fe462e19ea1bea99bcabe34c1618
Daniel Reszka <dannn.r@gmail.com>
2020-11-22 09:47:30 +0100

    pages/index: add auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { signIn, signOut, useSession } from "next-auth/client";
 import dynamic from "next/dynamic";
 import { Heading } from "../components/ui/Heading";
 import { UserList } from "../components/UserList";
@@ -9,8 +10,25 @@ const UserForm = dynamic(
 );
 
 export default function Home() {
+  const [session, loading] = useSession();
+
+  if (loading) return <>Loading...</>;
+
   return (
     <div>
+      {!session && (
+        <>
+          Not signed in <br />
+          <button onClick={() => signIn()}>Sign in</button>
+        </>
+      )}
+      {session && (
+        <>
+          Signed in as {session.user.email} <br />
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      )}
+
       <div className="flex flex-col text-center w-full">
         <Heading level={1}>Members</Heading>
       </div>
